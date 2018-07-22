6058dffb762fef5db6140919a7abf4ae0eeef6c1
Collin Gress <gress126@gmail.com>
2018-07-22 15:44:25 -0500

    test coverage is now reasonable

--- test/simulator.cc
+++ test/simulator.cc
@@ -123,6 +123,126 @@ TEST(SetTargetsFromAST, FindsAllTargetsInIncompleteAST) {
   ASSERT_TRUE(targets.back()->get_node()->is_multiplication());
 }
 
+TEST(Rollout, ResultsInValidAST) {
+  symreg::search_node node(std::make_unique<brick::AST::posit_node>());
+  symreg::MCTS::simulator::action_factory af(1);
+  
+  auto ast = symreg::MCTS::simulator::rollout(&node, 4, af);
+  ASSERT_TRUE(ast->is_full());
+  ASSERT_LE(ast->get_size(), 4);
+}
+
+TEST(AddActions, AddsNoActionsIfNoAvailableUpLinks) {
+  auto mab = symreg::MCTS::UCB1;
+  symreg::dataset ds;
+  ds.x = {1, 2, 3};
+  ds.y = {4, 5, 6};
+  auto loss = symreg::MCTS::bind_loss_fn(symreg::MCTS::NRMSD, ds); 
+  auto lp = symreg::MCTS::simulator::recursive_random_child_picker();
+
+  symreg::MCTS::simulator::simulator sim(mab, loss, lp, 4);
+
+  symreg::search_node root(std::make_unique<brick::AST::posit_node>());
+  root.add_child(std::make_unique<brick::AST::number_node>(1));
+  auto& one = root.get_children().front();
+  one.set_up_link(&root);
+  one.set_parent(&root);
+  ASSERT_FALSE(sim.add_actions(&one));
+}
+
+TEST(AddActions, AddsActionsAccordingToDepthLimit1) {
+  auto mab = symreg::MCTS::UCB1;
+  symreg::dataset ds;
+  ds.x = {1, 2, 3};
+  ds.y = {4, 5, 6};
+  auto loss = symreg::MCTS::bind_loss_fn(symreg::MCTS::NRMSD, ds); 
+  auto lp = symreg::MCTS::simulator::recursive_random_child_picker();
+
+  symreg::MCTS::simulator::simulator sim(mab, loss, lp, 4);
+
+  symreg::search_node root(std::make_unique<brick::AST::posit_node>());
+  root.add_child(std::make_unique<brick::AST::addition_node>());
+  root.set_depth(1);
+  auto& addition = root.get_children().front();
+  addition.set_up_link(&root);
+  addition.set_parent(&root);
+  addition.set_depth(2);
+  addition.set_unconnected(2);
+  ASSERT_TRUE(sim.add_actions(&addition));
+  for (auto& child : addition.get_children()) {
+    ASSERT_TRUE(child.get_ast_node()->is_terminal());
+  }
+}
+
+TEST(AddActions, AddsActionsAccordingToDepthLimit2) {
+  auto mab = symreg::MCTS::UCB1;
+  symreg::dataset ds;
+  ds.x = {1, 2, 3};
+  ds.y = {4, 5, 6};
+  auto loss = symreg::MCTS::bind_loss_fn(symreg::MCTS::NRMSD, ds); 
+  auto lp = symreg::MCTS::simulator::recursive_random_child_picker();
+
+  symreg::MCTS::simulator::simulator sim(mab, loss, lp, 9);
+
+  symreg::search_node root(std::make_unique<brick::AST::posit_node>());
+  root.add_child(std::make_unique<brick::AST::addition_node>());
+  root.set_depth(1);
+  auto& addition = root.get_children().front();
+  addition.set_up_link(&root);
+  addition.set_parent(&root);
+  addition.set_depth(2);
+  addition.set_unconnected(2);
+  ASSERT_TRUE(sim.add_actions(&addition));
+  symreg::MCTS::simulator::action_factory af(1);
+  ASSERT_EQ(addition.get_children().size(), af.get_set(100).size()); 
+}
+
+TEST(Simulate, ExpandsTreeIfPossible) {
+  auto mab = symreg::MCTS::UCB1;
+  symreg::dataset ds;
+  ds.x = {1, 2, 3};
+  ds.y = {4, 5, 6};
+  auto loss = symreg::MCTS::bind_loss_fn(symreg::MCTS::NRMSD, ds); 
+  auto lp = symreg::MCTS::simulator::recursive_random_child_picker();
+
+  symreg::MCTS::simulator::simulator sim(mab, loss, lp, 9);
+
+  symreg::search_node root(std::make_unique<brick::AST::posit_node>());
+  root.add_child(std::make_unique<brick::AST::addition_node>());
+  root.set_depth(1);
+  auto& addition = root.get_children().front();
+  addition.set_up_link(&root);
+  addition.set_parent(&root);
+  addition.set_depth(2);
+  addition.set_unconnected(2);
+
+  sim.simulate(&addition, 50);
+  ASSERT_GT(addition.get_children().size(), 0);
+}
+
+TEST(Simulate, DoesntExpandTreeIfDepthMaximized) {
+  auto mab = symreg::MCTS::UCB1;
+  symreg::dataset ds;
+  ds.x = {1, 2, 3};
+  ds.y = {4, 5, 6};
+  auto loss = symreg::MCTS::bind_loss_fn(symreg::MCTS::NRMSD, ds); 
+  auto lp = symreg::MCTS::simulator::recursive_random_child_picker();
+
+  symreg::MCTS::simulator::simulator sim(mab, loss, lp, 2);
+
+  symreg::search_node root(std::make_unique<brick::AST::posit_node>());
+  root.add_child(std::make_unique<brick::AST::number_node>(1));
+  root.set_depth(1);
+  auto& one = root.get_children().front();
+  one.set_up_link(&root);
+  one.set_parent(&root);
+  one.set_depth(2);
+  one.set_unconnected(0);
+
+  sim.simulate(&one, 50);
+  ASSERT_EQ(one.get_children().size(), 0);
+}
+
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
--- test/util.cc
+++ test/util.cc
@@ -3,11 +3,11 @@
 #include "symreg.hpp"
 #include "gtest/gtest.h"
 
-TEST(Stuff, WhoCares) {
-  
-  ASSERT_TRUE(true);
+TEST(GetRandomInt, ReturnsIntegerWithinRange) {
+  auto random = symreg::MCTS::util::get_random_int(0, 10, symreg::MCTS::mt);
+  ASSERT_GE(random, 0);
+  ASSERT_LE(random, 10);
 }
-
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
